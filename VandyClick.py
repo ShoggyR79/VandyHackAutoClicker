7da76dd77a73e81af1fea2c58a2d6d7813bfb17d
Du Duong <khaidu2002@gmail.com>
2020-10-04 13:25:40 -0500

    Finalize, Added Coins

--- VandyClick.py
+++ VandyClick.py
@@ -19,7 +19,7 @@ LIGHT_YELLOW = (218, 247, 166)
 smallFont = pygame.font.Font("PressStart2P.ttf", 15)
 mediumFont = pygame.font.Font("PressStart2P.ttf", 24)
 largeFont = pygame.font.Font("PressStart2P.ttf", 32)
-moveFont = pygame.font.Font("PressStart2P.ttf", 55)
+moveFont = pygame.font.Font("PressStart2P.ttf", 48)
 
 upgrade_cost_font = pygame.font.Font("PressStart2P.ttf", 10)
 upgrade_times_bought_font = pygame.font.Font("PressStart2P.ttf", 20)
@@ -28,10 +28,11 @@ pygame.display.set_caption("VandyCoin Clicker")
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 # Load Images
-coin_image = pygame.transform.scale(pygame.image.load(os.path.join("icons", "VandyCoin.png")), (250, 250))
+coin_image = pygame.transform.scale(pygame.image.load(os.path.join("icons", "VandyCoin.png")), (300, 300))
 
 BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("icons", "background.jpg")), (WIDTH, HEIGHT))
 
+PRIZE = pygame.transform.scale(pygame.image.load(os.path.join("icons", "Prize.png")), (250, 250))
 
 '''Buildings'''
 # follow the format below
@@ -40,14 +41,13 @@ cursor_img = pygame.transform.scale(pygame.image.load(os.path.join("icons", "Cur
 purse_img = pygame.transform.scale(pygame.image.load(os.path.join("icons", "Purse.png")), (300, 88))
 
 
-
 # set up coin class
 class MainCoin:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.width = 250
-        self.height = 250
+        self.width = 300
+        self.height = 300
 
         self.animation_state = 0
 
@@ -109,8 +109,6 @@ class Upgrades:
         return int(self.base_cost * self.cost_scaling**self.times_bought)
 
     def draw(self, solid):
-        # TODO: change the font of the upgrades
-
 
         icon = self.image.convert()
         cost = upgrade_cost_font.render(f"{self.get_total_cost()} Coins", 1, LIGHT_ORANGE)
@@ -138,10 +136,44 @@ class Player:
             self.coins_per_second += upgrade.coins_per_second * upgrade.times_bought
 
 
+# initialize the prize
+class Prize:
+    def __init__(self, x, y, image, cost):
+        self.x = x
+        self.y = y
+        self.cost = cost
+        self.width = 300
+        self.height = 300
+        self.image = image
+
+    def collide_point(self, mouse_pos):
+        return pygame.Rect(self.x, self.y, self.width, self.height).collidepoint(mouse_pos)
+
+    def get_total_cost(self):
+        return self.cost
+
+    def draw(self, solid):
+
+        icon = self.image.convert()
+        cost = upgrade_cost_font.render(f"{self.get_total_cost()} Coins", 1, LIGHT_ORANGE)
+        final_prize = largeFont.render("LIMITED", 1, LIGHT_ORANGE)
+        final_prize2 = largeFont.render("EDITION", 1, LIGHT_ORANGE)
+
+        if not solid:
+            icon.set_alpha(100)
+            screen.blit(final_prize, (self.x + 10, self.y + 10))
+            screen.blit(final_prize2, (self.x + 10, self.y + 60))
+        else:
+            icon.set_alpha(255)
+
+        screen.blit(icon, (self.x, self.y))
+        screen.blit(cost, (self.x + 150, self.y + self.height-30))
+
+
 coin = MainCoin(100, 100)
 coin_display = CoinDisplay(100, 0)
 user = Player()
-
+prize = Prize(700, HEIGHT - 500, PRIZE, 10)
 
 # constructing upgrades class
 base_y = HEIGHT
@@ -151,19 +183,31 @@ purse = Upgrades('Purse', 700, base_y - 88, purse_img, base_cost=100, cost_scali
 list_of_upgrades = [cursor, purse]
 
 
+def endgame():
+
+    screen.blit(BACKGROUND, (0, 0))
+    congrats = moveFont.render("Congratulation!", 1, (255, 255, 255))
+    congrats2 = moveFont.render("You have Bought the", 1, (255, 255, 255))
+    congrats3 = largeFont.render("Limited Edition VH Sticker!!", 1, (255, 255, 255))
+    screen.blit(congrats, (int((WIDTH/2) - 400), int(HEIGHT/2 - 150)))
+    screen.blit(congrats2, (int((WIDTH/2) - 460), int(HEIGHT/2)))
+    screen.blit(congrats3, (int((WIDTH/2) - 430), int((HEIGHT/2) + 150)))
+    pygame.display.update()
+
+
 # main loop
 def main():
     # set up run boolean to see if loop is running
     run = True
-    FPS = 60
+    fps = 60
     # coins = 0
     # coins_per_second = 0
     clock = pygame.time.Clock()
     channel_a = pygame.mixer.Channel(0)
     channel_b = pygame.mixer.Channel(1)
-    click_sound = pygame.mixer.Sound(os.path.join('audio', 'bgmusic.wav'))
-    channel_a.set_volume(0.5)
-    channel_a.play(click_sound, -1)
+    bg_sound = pygame.mixer.Sound(os.path.join('audio', 'bgmusic.wav'))
+    channel_a.set_volume(0.2)
+    channel_a.play(bg_sound, -1)
 
     def redraw_window():
         # Draw background
@@ -171,7 +215,10 @@ def main():
         # Draw coin
         coin.draw()
         coin_display.draw()
-
+        if user.coins >= prize.get_total_cost():
+            prize.draw(solid=True)
+        else:
+            prize.draw(solid=False)
         for upgrades in list_of_upgrades:
             if user.coins >= upgrades.get_total_cost():
                 upgrades.draw(solid=True)
@@ -185,7 +232,7 @@ def main():
 
     while run:
         # makes sure game run consistent with FPS
-        clock.tick(FPS)
+        clock.tick(fps)
         redraw_window()
         #
 
@@ -204,6 +251,13 @@ def main():
                         user.coins -= upgrade.get_total_cost()
                         upgrade.times_bought += 1
                         user.update_total_cps(list_of_upgrades)
+
+                if prize.collide_point(mouse_pos):
+                    if user.coins >= prize.get_total_cost():
+                        user.coins -= prize.get_total_cost()
+                        endgame()
+                        time.sleep(20)
+                        run = False
 
             if event.type == pygame.QUIT:
                 run = False
